d63bfb6d84920aa21135f345c99f447e760ebbf4
Shinichiro Suzuki <s.suzuki416@gmail.com>
2020-01-11 20:55:23 +0900

    Add mysql container and its settings

--- go/go.mod
+++ go/go.mod
@@ -2,4 +2,7 @@ module github.com/suzushin54/my-go-template
 
 go 1.12
 
-require github.com/gin-gonic/gin v1.5.0
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/labstack/gommon v0.3.0
+)
--- go/main.go
+++ go/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/labstack/gommon/log"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "hello!",
+			"message": "ping! pong!!",
 		})
 	})
-	r.Run(":8083")
+	err := r.Run(":8083")
+	if err != nil {
+		log.Error(err)
+	}
 }
